657fff317615bdd319caf787cbea1a3af9092800
Yunus Emre <topalogluyunusemre61@gmail.com>
2026-05-07 08:02:52 +0300

    added textnode_to_htmlnode function and it's tests(just one)

---
+++ src/test_textnode_to_htmlnode.py
@@ -0,0 +1,15 @@
+import unittest
+
+from textnode import TextNode, TextType
+from textnode_to_htmlnode import text_node_to_html_node
+
+
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.PLAIN_TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+if __name__ == "__main__":
+    unittest.main()
